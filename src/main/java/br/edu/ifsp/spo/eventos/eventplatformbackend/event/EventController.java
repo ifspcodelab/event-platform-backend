0eb123af138f059870cd555970e8434184f7d1ae
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-07-18 17:15:30 -0300

    feat: included validations in event create dto

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/event/EventController.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/event/EventController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/events")
 public class EventController {
     @PostMapping
-    public ResponseEntity<EventDto> create(@RequestBody EventCreateDto eventCreateDto) {
+    public ResponseEntity<EventDto> create(@Valid @RequestBody EventCreateDto eventCreateDto) {
         EventDto eventDto = new EventDto(
                 UUID.randomUUID(),
                 eventCreateDto.getTitle(),

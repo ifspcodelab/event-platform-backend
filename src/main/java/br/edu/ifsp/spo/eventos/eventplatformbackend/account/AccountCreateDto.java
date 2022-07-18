6a6e5e3b061a71333830451e1f66dc8a1ae203a7
rafaelspa <rsdepaula67@gmail.com>
2022-07-18 15:35:29 -0300

    feat: add lombok

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/AccountCreateDto.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/AccountCreateDto.java
@@ -1,5 +1,6 @@
 package br.edu.ifsp.spo.eventos.eventplatformbackend.account;
 
+import lombok.Value;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.Email;
@@ -7,70 +8,22 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Value
 public class AccountCreateDto {
-
     @NotNull
     @NotBlank
     @Size(min = 5, max = 256)
-    private String name;
+    String name;
     @NotNull
     @Email
-    private String email;
+    String email;
     @NotNull
     @CPF
-    private String cpf;
+    String cpf;
     @NotNull
     @NotBlank
     @Size(min = 8, max = 64)
-    private String password;
+    String password;
     @NotNull
-    private Boolean agreed;
-
-    public AccountCreateDto(String name, String email, String cpf, String password, Boolean agreed) {
-        this.name = name;
-        this.email = email;
-        this.cpf = cpf;
-        this.password = password;
-        this.agreed = agreed;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Boolean getAgreed() {
-        return agreed;
-    }
-
-    public void setAgreed(Boolean agreed) {
-        this.agreed = agreed;
-    }
+    Boolean agreed;
 }
--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/AccountDto.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/AccountDto.java
@@ -1,60 +1,16 @@
 package br.edu.ifsp.spo.eventos.eventplatformbackend.account;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
 public class AccountDto {
-
     private UUID id;
     private String name;
     private String email;
     private String cpf;
     private Boolean agreed;
-
-    public AccountDto(UUID id, String name, String email, String cpf, Boolean agreed) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.cpf = cpf;
-        this.agreed = agreed;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Boolean getAgreed() {
-        return agreed;
-    }
-
-    public void setAgreed(Boolean agreed) {
-        this.agreed = agreed;
-    }
 }

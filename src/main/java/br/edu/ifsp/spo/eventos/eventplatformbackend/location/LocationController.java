080486f03c0e884192e26d36d2cd8dfeff76749b
ana004 <paulaana08002019@gmail.com>
2022-07-18 10:09:48 -0300

    feat: create location controller, dto and exception handler

---
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationController.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.spo.eventos.eventplatformbackend.location;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+public class LocationController {
+
+    @RequestMapping(value = "api/v1/locations")
+    @PostMapping
+    public LocationDto create(@RequestBody @Valid LocationCreateDto locationCreateDto) {
+        LocationDto locationDto = new LocationDto(UUID.randomUUID(), locationCreateDto.getName(), locationCreateDto.getAddress());
+        return locationDto;
+    }
+}

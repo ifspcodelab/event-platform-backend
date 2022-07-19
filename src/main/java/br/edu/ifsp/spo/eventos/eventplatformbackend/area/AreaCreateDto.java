ad5438b9a40dc26bbcd4f719020161db99ba2f9f
isabella-66 <amasprog@gmail.com>
2022-07-19 09:32:32 -0300

    test: add test to area create dto reference

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaCreateDto.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaCreateDto.java
@@ -9,10 +9,11 @@ public class AreaCreateDto {
     @NotNull
     @NotBlank
     @Size(min = 4, max = 80)
+    @Pattern(regexp="^[0-9A-Za-záàâãéèêíóôõúçñÁÀÂÃÉÈÍÓÔÕÚÇ,:.-[\\s]]*$", message = "Campo nome deve conter apenas letras e números")
     String name;
 
     //TODO: definir regexp que aceita apenas letras e números
-    //@Pattern(regexp="^[a-z0-9]*[a-z]+[a-z0-9]*$", message = "Campo referência deve conter apenas letras e números")
+    @Pattern(regexp="^[0-9A-Za-záàâãéèêíóôõúçñÁÀÂÃÉÈÍÓÔÕÚÇ,:.-[\\s]]*$", message = "Campo referência deve conter apenas letras e números")
     @Size(min = 4, max = 150)
     String reference;
 }
---
+++ src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaCreateDtoTest.java
@@ -0,0 +1,82 @@
+package br.edu.ifsp.spo.eventos.eventplatformbackend.area;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.validation.*;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AreaCreateDtoTest {
+    Validator validator;
+
+    @BeforeEach
+    public void before() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    //AAA
+    @Test
+    public void areaCreateDtoValidReference() {
+        // Arrange
+        AreaCreateDto areaCreateDto = new AreaCreateDto("Bloco C", "Piso Superior");
+
+        // Act
+        Set<ConstraintViolation<AreaCreateDto>> violations = validator.validate(areaCreateDto);
+
+        // Assert
+        assertThat(violations).hasSize(0);
+    }
+
+    @Test
+    public void areaCreateDtoInvalidReferenceOnlySpaces() {
+        // Arrange
+        AreaCreateDto areaCreateDto = new AreaCreateDto("Bloco C", "   ");
+
+        // Act
+        Set<ConstraintViolation<AreaCreateDto>> violations = validator.validate(areaCreateDto);
+
+        // Assert
+        assertThat(violations).hasSize(1);
+        assertThat(violations)
+                .extracting(ConstraintViolation::getPropertyPath)
+                .map(Path::toString)
+                .first()
+                .isEqualTo("reference");
+    }
+
+    @ParameterizedTest(name = "Invalid reference value {0}")
+    @ValueSource(strings = { "   ", "$#@ *", "Piso superior?", "Piso superior!" })
+    void areaCreateDtoInvalidReference(String reference) {
+        // Arrange
+        AreaCreateDto areaCreateDto = new AreaCreateDto("Bloco C", reference);
+
+        // Act
+        Set<ConstraintViolation<AreaCreateDto>> violations = validator.validate(areaCreateDto);
+
+        // Assert
+        assertThat(violations).hasSize(1);
+        assertThat(violations)
+                .extracting(ConstraintViolation::getPropertyPath)
+                .map(Path::toString)
+                .first()
+                .isEqualTo("reference");
+    }
+
+    @ParameterizedTest(name = "Valid reference value {0}")
+    @ValueSource(strings = { "Piso superior", "Piso superior 5.", "Piso-superior", "Piso: superior", "448, 98" })
+    void areaCreateDtoValidReference(String reference) {
+        // Arrange
+        AreaCreateDto areaCreateDto = new AreaCreateDto("Bloco C", reference);
+
+        // Act
+        Set<ConstraintViolation<AreaCreateDto>> violations = validator.validate(areaCreateDto);
+
+        // Assert
+        assertThat(violations).hasSize(0);
+    }
+}

cd0c17c87d5ff4c018858578eb4ebf19ab53cf2f
rafaelspa <rsdepaula67@gmail.com>
2022-07-19 17:58:17 -0300

    refactor: refactor migration file

--- src/main/resources/db/migration/V5__create_accounts_table.sql
+++ src/main/resources/db/migration/V5__create_accounts_table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE accounts(
     id UUID NOT NULL,
-    nome VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
     cpf VARCHAR NOT NULL UNIQUE,
     password VARCHAR NOT NULL,
     agreed BOOLEAN NOT NULL,
+    role VARCHAR NOT NULL,
     CONSTRAINT accounts_pk PRIMARY KEY (id)
 )

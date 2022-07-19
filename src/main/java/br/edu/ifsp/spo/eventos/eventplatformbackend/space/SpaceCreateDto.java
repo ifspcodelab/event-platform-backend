dad994aec0b058e11d42291b413c4d1b648f23ae
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-07-18 22:37:10 -0300

    feat: add lombok to space dtos

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/space/SpaceCreateDto.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/space/SpaceCreateDto.java
@@ -1,49 +1,23 @@
 package br.edu.ifsp.spo.eventos.eventplatformbackend.space;
 
+import lombok.Value;
+
 import javax.validation.constraints.*;
 
+@Value
 public class SpaceCreateDto {
 
     @NotNull
     @NotBlank
     @Size (min=4, max=100)
     @Pattern(regexp = "^[0-9A-Za-záàâãéèêíóôõúçñÁÀÂÃÉÈÍÓÔÕÚÇ,.\\s-]*$", message = "Campo nome deve conter apenas letras e números")
-    private String name;
+    String name;
     @NotNull
     @Min(value = 2)
     @Max(value = 9999)
-    private Integer capacity;
+    Integer capacity;
     //TODO: Validar tipos que não existem
     @NotNull
-    private SpaceType type;
+    SpaceType type;
 
-    public SpaceCreateDto(String name, Integer capacity, SpaceType type) {
-        this.name = name;
-        this.capacity = capacity;
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public SpaceType getType() {
-        return type;
-    }
-
-    public void setType(SpaceType type) {
-        this.type = type;
-    }
 }
--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/space/SpaceDto.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/space/SpaceDto.java
@@ -1,7 +1,12 @@
 package br.edu.ifsp.spo.eventos.eventplatformbackend.space;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
 public class SpaceDto {
 
     private UUID id;
@@ -9,42 +14,4 @@ public class SpaceDto {
     private Integer capacity;
     private SpaceType type;
 
-    public SpaceDto(UUID id, String name, Integer capacity, SpaceType type) {
-        this.id = id;
-        this.name = name;
-        this.capacity = capacity;
-        this.type = type;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public SpaceType getType() {
-        return type;
-    }
-
-    public void setType(SpaceType type) {
-        this.type = type;
-    }
 }

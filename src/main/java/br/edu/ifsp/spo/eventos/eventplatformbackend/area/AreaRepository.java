0a4a959282d4f6fd59bd4b7cc767e0fdca6cbf43
isabella-66 <amasprog@gmail.com>
2022-07-19 21:08:20 -0300

    feat: add area and location repositories

---
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.spo.eventos.eventplatformbackend.area;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AreaRepository extends JpaRepository<Area, UUID> {
+}
---
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.spo.eventos.eventplatformbackend.location;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, UUID> {
+}

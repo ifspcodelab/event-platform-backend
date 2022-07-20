a00068d418fb3a5818f0d8f9aeee6823eb42c3b1
ana004 <paulaana08002019@gmail.com>
2022-07-20 20:19:01 -0300

    feat: add map to index method

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationRepository.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationRepository.java
@@ -3,9 +3,11 @@ package br.edu.ifsp.spo.eventos.eventplatformbackend.location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, UUID> {
     boolean existsByName(String name);
+    List<Location> findAll();
 }

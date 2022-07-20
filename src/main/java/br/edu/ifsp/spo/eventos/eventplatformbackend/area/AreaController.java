ade12abb1e64db88e15591f19dc5229724746536
isabella-66 <amasprog@gmail.com>
2022-07-20 11:03:48 -0300

    feat: update stream area find all by location id

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaController.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/locations/{locationId}/areas")
@@ -28,10 +29,9 @@ public class AreaController {
     @GetMapping
     public ResponseEntity<List<AreaDto>> index(@PathVariable UUID locationId) {
         List<Area> areas = areaService.findAll(locationId);
-        List<AreaDto> areasDto = new ArrayList<>();
-        for (Area area : areas) {
-            areasDto.add(areaMapper.to(area));
-        }
+        List<AreaDto> areasDto = areas.stream()
+                .map(area -> areaMapper.to(area))
+                .collect(Collectors.toList());
         return new ResponseEntity<>(areasDto, HttpStatus.OK);
     }
 

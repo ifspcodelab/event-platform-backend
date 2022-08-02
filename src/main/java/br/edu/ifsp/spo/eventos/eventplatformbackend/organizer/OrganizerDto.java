7ab98830e38ab55c5edc73033beefb02c517d015
isabella-66 <amasprog@gmail.com>
2022-08-02 20:14:34 -0300

    feat: add organizer enum type and dtos

---
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/organizer/OrganizerDto.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.spo.eventos.eventplatformbackend.organizer;
+
+import br.edu.ifsp.spo.eventos.eventplatformbackend.account.AccountDto;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.event.EventDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class OrganizerDto {
+    UUID id;
+    OrganizerType type;
+    AccountDto account;
+    EventDto event;
+}

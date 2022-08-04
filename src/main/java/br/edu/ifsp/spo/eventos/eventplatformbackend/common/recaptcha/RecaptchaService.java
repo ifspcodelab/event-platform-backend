ab481366b7eeedba9bcd19e67ae2a4bb6456e030
rafaelspa <rsdepaula67@gmail.com>
2022-08-04 14:09:11 -0300

    feat: add error handling in recaptcha service

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/common/recaptcha/RecaptchaService.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/common/recaptcha/RecaptchaService.java
@@ -4,6 +4,7 @@ import br.edu.ifsp.spo.eventos.eventplatformbackend.common.recaptcha.config.Reca
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -14,7 +15,12 @@ public class RecaptchaService {
     private RecaptchaConfig config;
 
     public boolean isValid(String recaptcha){
-        return restTemplate.getForObject(buildRequestURL(recaptcha), RecaptchaResponseDto.class).isSuccess();
+        try{
+            return restTemplate.getForObject(buildRequestURL(recaptcha), RecaptchaResponseDto.class).isSuccess();
+        } catch (RestClientException ex) {
+            log.warn(ex.getMessage(), ex);
+            return true;
+        }
     }
 
     public String buildRequestURL(String recaptcha){

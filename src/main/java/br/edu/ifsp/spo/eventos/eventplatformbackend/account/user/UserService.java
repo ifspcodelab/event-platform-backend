d244e4c1fe2d45fdeb95d42f71cc80bd8d1d65e2
Lucas Gabriel <lucasogjc@gmail.com>
2022-08-11 13:33:46 -0300

    feat: implements getuserbyaccesstoken method

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/user/UserService.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/user/UserService.java
@@ -1,18 +1,38 @@
 package br.edu.ifsp.spo.eventos.eventplatformbackend.account.user;
 
+import br.edu.ifsp.spo.eventos.eventplatformbackend.account.Account;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.account.AccountRepository;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.account.authentication.AuthenticationException;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.account.authentication.AuthenticationExceptionType;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.common.security.JwtService;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class UserService {
+    private final JwtService jwtService;
+    private final AccountRepository accountRepository;
+    
     public MyDataDto getUserByAccessToken(String accessToken) {
-        String name = "Gabriel menino";
-        String email = "gabrielmenino@mail.com";
-        String cpf = "330.657.891-55";
+        DecodedJWT decodedToken = jwtService.decodeToken(accessToken);
+        UUID accountId = UUID.fromString(decodedToken.getSubject());
 
-        MyDataDto myDataDto = new MyDataDto(name, email, cpf);
+        Account account = getAccount(accountId);
+        
+        String name = account.getName();
+        String email = account.getEmail();
+        String cpf = account.getCpf();
 
-        return myDataDto;
+        return new MyDataDto(name, email, cpf);
+    }
+
+    private Account getAccount(UUID id) {
+        return accountRepository.findById(id).orElseThrow(
+                () -> new AuthenticationException(AuthenticationExceptionType.NONEXISTENT_ACCOUNT_BY_ID, id.toString())
+        );
     }
 }

8871024b53a3e8ae363396f19c00876533dee888
Domingos Latorre <domingos.latorre@gmail.com>
2022-08-19 10:09:02 -0300

    feat: add speaker search by name

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/speaker/SpeakerService.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/speaker/SpeakerService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -59,6 +60,10 @@ public class SpeakerService {
 
     public Page<Speaker> findAll(Pageable pageable) {
         return speakerRepository.findAll(pageable);
+    }
+
+    public List<Speaker> findByName(String name) {
+        return speakerRepository.findByNameStartingWithIgnoreCase(name);
     }
 
     public Speaker update(UUID speakerId, SpeakerCreateDto dto) {

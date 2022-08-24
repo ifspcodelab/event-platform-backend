aafe9e30b23aa4e5faba74687db0acf0dcb502ee
Lucas Gabriel <lucasogjc@gmail.com>
2022-08-24 16:41:29 -0300

    feat: adds apache diff dependencies and implements it on account model

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/Account.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/Account.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.DiffBuilder;
+import org.apache.commons.lang3.builder.DiffResult;
+import org.apache.commons.lang3.builder.Diffable;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import javax.persistence.*;
 import java.time.Instant;
@@ -15,7 +19,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Setter
-public class Account {
+public class Account implements Diffable<Account> {
     @Id
     private UUID id;
     private String name;
@@ -38,5 +42,14 @@ public class Account {
         this.role = AccountRole.ATTENDANT;
         this.verified = false;
         this.registrationTimestamp = Instant.now();
+    }
+
+    @Override
+    public DiffResult<Account> diff(Account updatedAccount) {
+        return new DiffBuilder<>(this, updatedAccount, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("Nome", this.name, updatedAccount.name)
+                .append("email", this.email, updatedAccount.email)
+                .append("CPF", this.cpf, updatedAccount.cpf)
+                .build();
     }
 }

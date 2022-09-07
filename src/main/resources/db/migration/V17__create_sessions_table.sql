8f6764319cb393f63d9bff3885ecd043bc5d7cc9
Domingos Latorre <domingos.latorre@gmail.com>
2022-09-07 05:40:45 -0300

    fix: session table migration

--- src/main/resources/db/migration/V17__create_sessions_table.sql
+++ src/main/resources/db/migration/V17__create_sessions_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE sessions(
-       id UUID NOT NULL,
-       title VARCHAR NOT NULL,
-       seats SMALLINT NOT NULL,
-       cancellation_message TEXT,
-       canceled BOOLEAN NOT NULL,
-       activity_id UUID NOT NULL,
-       CONSTRAINT sessions_pk PRIMARY KEY (id),
-       CONSTRAINT activities_fk FOREIGN KEY (activity_id) REFERENCES activities(id)
+    id UUID NOT NULL,
+    title VARCHAR NOT NULL,
+    seats SMALLINT NOT NULL,
+    cancellation_message TEXT,
+    canceled BOOLEAN NOT NULL,
+    activity_id UUID NOT NULL,
+    CONSTRAINT sessions_pk PRIMARY KEY (id),
+    CONSTRAINT activities_fk FOREIGN KEY (activity_id) REFERENCES activities(id)
 )

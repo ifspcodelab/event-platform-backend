08d26595b8b55f6704b775ddf4426d6391704e19
Lucas Gabriel <lucasogjc@gmail.com>
2022-11-21 16:02:45 -0300

    test: create area throws already exists exception

--- src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaServiceTest.java
+++ src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaServiceTest.java
@@ -1,6 +1,8 @@
 package br.edu.ifsp.spo.eventos.eventplatformbackend.area;
 
 import br.edu.ifsp.spo.eventos.eventplatformbackend.account.audit.AuditService;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceAlreadyExistsException;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.location.Location;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.location.LocationRepository;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.space.SpaceRepository;
 import org.junit.jupiter.api.Test;
@@ -9,7 +11,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AreaServiceTest {
@@ -27,5 +35,28 @@ public class AreaServiceTest {
     @Test
     public void areaServiceShouldNotBeNull() {
         assertThat(areaService).isNotNull();
+    }
+
+    @Test
+    public void create_ThrowsException_WhenAreaAlreadyExists() {
+        AreaCreateDto areaCreateDto = new AreaCreateDto(
+                "Bloco A",
+                "Piso Superior"
+        );
+        Location location = new Location(
+                UUID.randomUUID(),
+                "IFSP Campus São Paulo",
+                "R. Pedro Vicente, 625 - Canindé, São Paulo - SP, 01109-010"
+        );
+
+        when(locationRepository.findById(any(UUID.class)))
+                .thenReturn(Optional.of(location));
+        when(areaRepository.existsByNameAndLocationId(
+                areaCreateDto.getName(),
+                location.getId())
+        ).thenReturn(true);
+
+        assertThatThrownBy(() -> areaService.create(location.getId(), areaCreateDto))
+                .isInstanceOf(ResourceAlreadyExistsException.class);
     }
 }

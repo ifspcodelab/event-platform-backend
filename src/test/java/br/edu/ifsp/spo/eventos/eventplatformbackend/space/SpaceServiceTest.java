04b0457e6bbe2aa565a1003875d364ac77037aba
rafaelspa <rsdepaula67@gmail.com>
2022-11-22 15:29:43 -0300

    test: create space resource not found exception in getarea method

--- src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/space/SpaceServiceTest.java
+++ src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/space/SpaceServiceTest.java
@@ -3,28 +3,22 @@ package br.edu.ifsp.spo.eventos.eventplatformbackend.space;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.account.audit.AuditService;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.area.Area;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.area.AreaRepository;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceAlreadyExistsException;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceNotExistsAssociationException;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceNotFoundException;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.location.Location;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpaceServiceTest {
@@ -40,5 +34,16 @@ class SpaceServiceTest {
     @Test
     public void spaceServiceShouldNotBeNull() {
         assertThat(spaceService).isNotNull();
+    }
+
+    @Test
+    public void create_ResourceNotFoundExceptionException_WhenThereIsNoAreaPersisted() {
+        SpaceCreateDto dto = new SpaceCreateDto("nome", 123, SpaceType.AUDITORIUM);
+
+        UUID randomLocationUuid = UUID.randomUUID();
+        UUID randomAreaUuid = UUID.randomUUID();
+
+        assertThatThrownBy(() -> spaceService.create(randomLocationUuid, randomAreaUuid , dto))
+                .isInstanceOf(ResourceNotFoundException.class);
     }
 }

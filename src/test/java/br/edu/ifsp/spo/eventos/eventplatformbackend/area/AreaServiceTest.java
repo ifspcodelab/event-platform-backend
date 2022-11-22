0d0c267a9077f638d39f2e14101a954173f588d8
Lucas Gabriel <lucasogjc@gmail.com>
2022-11-22 15:35:35 -0300

    test: delete area throws location is not associated exception

--- src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaServiceTest.java
+++ src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/area/AreaServiceTest.java
@@ -233,4 +233,26 @@ public class AreaServiceTest {
         assertThatThrownBy(() -> areaService.delete(locationId, areaId))
                 .isInstanceOf(ResourceNotFoundException.class);
     }
+
+    @Test
+    public void delete_ThrowsException_WhenLocationDoesNotExistInGivenArea() {
+        Location location = new Location(
+                UUID.randomUUID(),
+                "IFSP Campus São Paulo",
+                "R. Pedro Vicente, 625 - Canindé, São Paulo - SP, 01109-010"
+        );
+        Area area = new Area(
+                "Bloco A",
+                "Piso Superior",
+                location
+        );
+        UUID locationId = UUID.randomUUID();
+        UUID areaId = area.getId();
+
+        when(areaRepository.findById(any(UUID.class)))
+                .thenReturn(Optional.of(area));
+
+        assertThatThrownBy(() -> areaService.delete(locationId, areaId))
+                .isInstanceOf(ResourceNotExistsAssociationException.class);
+    }
 }

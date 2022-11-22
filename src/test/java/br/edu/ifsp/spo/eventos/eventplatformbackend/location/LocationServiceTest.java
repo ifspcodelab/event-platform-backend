9576db3636c70400d7e19b66a0657785a13e951e
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-11-22 16:41:19 -0300

    test: add tests to method of deleting location

--- src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationServiceTest.java
+++ src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationServiceTest.java
@@ -5,7 +5,7 @@ import br.edu.ifsp.spo.eventos.eventplatformbackend.area.AreaRepository;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceAlreadyExistsException;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceName;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceNotFoundException;
-import org.assertj.core.api.InstanceOfAssertFactories;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.ResourceReferentialIntegrityException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -39,9 +39,7 @@ public class LocationServiceTest {
         when(locationRepository.existsByName(any(String.class))).thenReturn(true);
 
         assertThatThrownBy(() -> locationService.create(locationCreateDto))
-                .isInstanceOf(ResourceAlreadyExistsException.class)
-                .extracting("resourceName", InstanceOfAssertFactories.type(ResourceName.class))
-                .isEqualTo(ResourceName.LOCATION);
+                .isInstanceOf(ResourceAlreadyExistsException.class);
     }
 
     @Test
@@ -76,9 +74,7 @@ public class LocationServiceTest {
         when(locationRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
 
         assertThatThrownBy(() -> locationService.update(locationId, locationCreateDto))
-                .isInstanceOf(ResourceNotFoundException.class)
-                .extracting("resourceName", InstanceOfAssertFactories.type(ResourceName.class))
-                .isEqualTo(ResourceName.LOCATION);
+                .isInstanceOf(ResourceNotFoundException.class);
     }
     @Test
     public void update_ThrowsException_WhenLocationNameAlreadyExistsExcludingTheProvided() {
@@ -95,9 +91,7 @@ public class LocationServiceTest {
         when(locationRepository.existsByNameAndIdNot(any(String.class),any(UUID.class))).thenReturn(true);
 
         assertThatThrownBy(() -> locationService.update(locationId, locationCreateDto))
-                .isInstanceOf(ResourceAlreadyExistsException.class)
-                .extracting("resourceName", InstanceOfAssertFactories.type(ResourceName.class))
-                .isEqualTo(ResourceName.LOCATION);
+                .isInstanceOf(ResourceAlreadyExistsException.class);
     }
 
     @Test
@@ -120,10 +114,53 @@ public class LocationServiceTest {
         verify(auditService, times(1)).logAdminUpdate(
                 any(ResourceName.class),
                 any(String.class),
-                any(UUID.class));
+                any(UUID.class)
+        );
         assertThat(locationUpdated).isNotNull();
         assertThat(locationUpdated.getId()).isEqualTo(location.getId());
         assertThat(locationUpdated.getName()).isEqualTo(location.getName());
         assertThat(locationUpdated.getAddress()).isEqualTo(location.getAddress());
+    }
+
+    @Test
+    public void delete_ThrowsException_WhenLocationDoesNotExists() {
+        UUID locationId = UUID.randomUUID();
+        when(locationRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> locationService.delete(locationId))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    public void delete_ThrowsException_WhenLocationHasArea() {
+        Location location = new Location(
+                "Shopping D",
+                "Av. Cruzeiro do Sul"
+        );
+        UUID locationId = UUID.randomUUID();
+        when(locationRepository.findById(any(UUID.class))).thenReturn(Optional.of(location));
+        when(areaRepository.existsByLocationId(any(UUID.class))).thenReturn(true);
+
+        assertThatThrownBy(() -> locationService.delete(locationId))
+                .isInstanceOf(ResourceReferentialIntegrityException.class);
+    }
+
+    @Test
+    public void delete_DeletesLocation_WhenSuccessful() {
+        Location location = new Location(
+                "Shopping D",
+                "Av. Cruzeiro do Sul"
+        );
+        UUID locationId = UUID.randomUUID();
+        when(locationRepository.findById(any(UUID.class))).thenReturn(Optional.of(location));
+        when(areaRepository.existsByLocationId(any(UUID.class))).thenReturn(false);
+
+        locationService.delete(locationId);
+
+        verify(locationRepository, times(1)).deleteById(any(UUID.class));
+        verify(auditService, times(1)).logAdminDelete(
+                any(ResourceName.class),
+                any(UUID.class)
+        );
     }
 }

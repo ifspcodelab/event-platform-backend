665ee0d6f9f385e90d5f2be890509726b71521a6
Lucas Gabriel <lucasogjc@gmail.com>
2022-12-01 17:46:55 -0300

    test: create reset password request throws invalid recaptcha exception

--- src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/password/PasswordResetServiceTest.java
+++ src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/account/password/PasswordResetServiceTest.java
@@ -4,6 +4,8 @@ import br.edu.ifsp.spo.eventos.eventplatformbackend.account.AccountConfig;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.account.AccountRepository;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.account.audit.AuditService;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.common.email.EmailService;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.RecaptchaException;
+import br.edu.ifsp.spo.eventos.eventplatformbackend.common.exceptions.RecaptchaExceptionType;
 import br.edu.ifsp.spo.eventos.eventplatformbackend.common.recaptcha.RecaptchaService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,7 +14,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PasswordResetServiceTest {
@@ -36,5 +42,24 @@ public class PasswordResetServiceTest {
     @Test
     public void passwordResetService_ShouldNotBeNull() {
         assertThat(passwordResetService).isNotNull();
+    }
+
+    @Test
+    public void createResetPasswordRequest_ThrowsException_WhenRecaptchaIsInvalid() {
+        ForgotPasswordCreateDto forgotPasswordCreateDto = sampleForgotPasswordCreateDto();
+        when(recaptchaService.isValid(anyString())).thenReturn(Boolean.FALSE);
+
+        RecaptchaException exception = (RecaptchaException) catchThrowable(() -> passwordResetService.createResetPasswordRequest(forgotPasswordCreateDto));
+
+        assertThat(exception).isInstanceOf(RecaptchaException.class);
+        assertThat(exception.getRecaptchaExceptionType()).isEqualTo(RecaptchaExceptionType.INVALID_RECAPTCHA);
+        assertThat(exception.getEmail()).isEqualTo(forgotPasswordCreateDto.getEmail());
+    }
+
+    private ForgotPasswordCreateDto sampleForgotPasswordCreateDto() {
+        return new ForgotPasswordCreateDto(
+                "shineinouzen@email.com",
+                UUID.randomUUID().toString()
+        );
     }
 }

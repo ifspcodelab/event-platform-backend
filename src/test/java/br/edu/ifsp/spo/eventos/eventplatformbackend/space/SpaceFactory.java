8b0e8820e94086438b96a4572740cf6ca1f4cd45
rafaelspa <rsdepaula67@gmail.com>
2022-12-06 08:07:22 -0300

    test: add factories and config

---
+++ src/test/java/br/edu/ifsp/spo/eventos/eventplatformbackend/space/SpaceFactory.java
@@ -0,0 +1,26 @@
+package br.edu.ifsp.spo.eventos.eventplatformbackend.space;
+
+import br.edu.ifsp.spo.eventos.eventplatformbackend.area.AreaFactory;
+
+import java.util.UUID;
+
+public class SpaceFactory {
+    public static Space sampleSpace() {
+        return new Space(
+                "IVO",
+                100,
+                SpaceType.AUDITORIUM,
+                AreaFactory.sampleArea()
+        );
+    }
+
+    public static Space sampleSpaceWithHardcodedUuid() {
+        return new Space(
+                UUID.fromString("8215f714-1bd5-4a17-bbef-6aa9396775a8"),
+                "IVO",
+                100,
+                SpaceType.AUDITORIUM,
+                AreaFactory.sampleAreaWithHardcodedLocationUuid()
+        );
+    }
+}

588cfccc31c6ea3f591083821c820f968bc959e4
ana004 <paulaana08002019@gmail.com>
2022-12-18 14:06:04 -0300

    fix: add pattern regexp for numbers and special characteres to name and address fields

--- src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationCreateDto.java
+++ src/main/java/br/edu/ifsp/spo/eventos/eventplatformbackend/location/LocationCreateDto.java
@@ -3,15 +3,18 @@ package br.edu.ifsp.spo.eventos.eventplatformbackend.location;
 import lombok.Value;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Value
 public class LocationCreateDto {
+    @Pattern(regexp = "^(?![0-9]+$)[0-9a-zA-ZÀ-ü][0-9a-zA-ZÀ-ü ]*$")
     @NotNull
     @NotBlank
     @Size(min = 1, max = 200)
     String name;
 
+    @Pattern(regexp = "^(?![0-9]+$)[0-9a-zA-ZÀ-ü][0-9a-zA-ZÀ-ü ]*$")
     @NotNull
     @NotBlank
     @Size(min = 10, max = 300)
